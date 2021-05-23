517fd88b71e592d2bc4c41cdc40bfcfbc076590b
StellaD5319 <dazzlestars5319@gmail.com>
2021-05-24 03:15:46 +1000

    MapReduce results.

---
+++ harvest/search_tweet_interface.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python
+
+"""
+This code is used to search the tweets in certain regions from start_date to end_date.
+
+"""
+import tweepy
+from tweepy import API
+from tweepy import OAuthHandler
+import datetime
+import os, json, sys, argparse
+from utils import dbOperations
+import time
+import datetime
+
+#--------------------------------------------------------------------------------#
+##   Global variable define
+#--------------------------------------------------------------------------------#
+TWITTER_API_PATH = "config/twitter_api.json"
+REGION_GEO = './config/regions.json'
+CITIES = ["Greater_Sydney", "Greater_Melbourne", "Greater_Brisbane", "Greater_Perth", "Greater_Adelaide"]
+
+#--------------------------------------------------------------------------------#
+##   Global variable catch.
+#--------------------------------------------------------------------------------#
+with open(TWITTER_API_PATH, "r") as key_f:
+    TOKENS_TW = json.load(key_f)
+with open(REGION_GEO, "r") as geo_f:
+    GEO_AREAS = json.load(geo_f)
+#--------------------------------------------------------------------------------#
+##   Class define
+#--------------------------------------------------------------------------------#
+class search_API(object):
+    def __init__(self, start_time, end_time, db_server, db_name, token_idx=0):
+        self.token_idx = token_idx
+        self.server = db_server
+        self.db_name = db_name
+        self.begin = start_time
+        self.end = end_time
+
+    def token_get(self, token_index=0):
+        return TOKENS_TW[token_index]
+
+    def _build_pipe(self, token):
+        auth = OAuthHandler(token["consumer_key"], token["consumer_secret"])
+        auth.set_access_token(token["access_token"], token["access_secret"])
+        return auth
+
+    def area_to_place_id(self, city_name):
+        name = city_name.lower()
+        if name in GEO_AREAS.keys():
+            return GEO_AREAS[name]["location_id"]
+        else:
+            print("[-] ERROR not found the city we want to search!!")
+            sys.exit(0)
+
+    def search_twitters(self, area="australia"):
+        token = self.token_get(self.token_idx)
+        auth = self._build_pipe(token)
+        api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+        if area == "australia":
+            place_id = "3f14ce28dc7c4566"
+        else:
+            place_id = self.area_to_place_id(area)
+
+        public_tweets = tweepy.Cursor(api.search,
+                                            q="place:%s -filter:retweets" % place_id,
+                                            since=self.begin,
+                                            until=self.end).items(1000000)
+        return public_tweets
+
+def save_tweets(db_server, db_name, tweets):
+    while True:
+        try:
+            tweet = tweets.next()
+            # Insert into db
+            db_server.save_views_into_tables(db_name, tweet._json)
+        except tweepy.TweepError:
+            time.sleep(60 * 15)
+            continue
+        except StopIteration:
+            break
+
+def get_opts():
+    """
+    Getting the input file.
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--city', type=str,
+                        default='australia',
+                        help='search the position')
+    parser.add_argument('--duration', type=str,
+                        default='7',
+                        help='search the period time of twitters')
+    parser.add_argument('--token_id', type=str,
+                        default='0',
+                        help='use one token. total 5 tokens')
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    """
+    Set the different cases:
+    case1: python search_tweet_interface.py 
+           default crawler the australia twitters. save into dbname as "whole"
+           default search period is 7 days due to twitter limitation.
+    case2: python search_tweet_interface.py city_name
+           search certain city, in 7 days.
+    case3:  python search_tweet_interface.py city_name
+    """
+
+    #db_ip = os.getenv("COUCH_DB_IP")
+    db_ip = "172.26.129.170"
+    dbname = "au_tweets"
+    hparams = get_opts()
+    start = datetime.date.today() - datetime.timedelta(days=int(hparams.duration))
+    end = datetime.date.today()
+    # connect the databases
+    client = dbOperations(db_ip=db_ip, db_port=5984, db_username="ccc58", db_passwd="ccc58", databases=[dbname])
+
+    search = search_API(start_time=start, end_time=end, db_server=client,
+                        db_name=dbname, token_idx=hparams.token_id)
+    tweets = search.search_twitters(hparams.city)
+    save_tweets(client, dbname, tweets)
+    print("[+] Saving the all tweets into couchdb.")
+
+
+
+
+
+
+
---
+++ harvest/stream_tweet_interface.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+
+"""
+This code is used to stream the tweets in certain regions.
+"""
+
+import tweepy
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+import json
+import sys, os
+from utils import dbOperations
+import datetime as dt
+import couchdb
+import http.client
+
+# put different consumer_keys
+def _get_twitter_api(token_idx='0'):
+    #set up twitter authentication
+    # Return: tweepy.OAuthHandler object
+
+    twitter_api_file = './config/twitter_api.json'
+    with open(twitter_api_file, "r") as f:
+        tokens = json.load(f)
+
+    auth = OAuthHandler(tokens[token_idx]["consumer_key"], tokens[token_idx]["consumer_secret"])
+    auth.set_access_token(tokens[token_idx]["access_token"], tokens[token_idx]["access_secret"])
+    return auth
+
+def _get_coordinates_base_city(city):
+    region_geo = './config/regions.json'
+    locations = []
+    with open(region_geo, 'r') as f:
+        city_geo = json.load(f)
+        try:
+            locations = city_geo[city]["coordinate"]
+        except Exception as e:
+            print("[-] ERROR : some error happen, ", e)
+    return locations
+
+
+@classmethod
+def parse(cls, api, raw):
+    status = cls.first_parse(api, raw)
+    setattr(status, 'json', json.dumps(raw))
+    return status
+
+class MyListener(StreamListener):
+    def __init__(self, db_server, db_name):
+        self.db_name = db_name
+        self.server = db_server
+
+    def on_data(self, data):
+        try:
+            self.server.save_views_into_tables(self.db_name, json.loads(data))
+        except AttributeError:
+            print("[-] ERROR : some error happen, ", data)
+        except http.client.IncompleteRead as e:
+            print(e.partial)
+            file = e.partial
+        return True
+
+    def on_error(self, status):
+        print(status)
+        return True
+
+def get_twitters_stream(location, db_server, db_name):
+    # Set the hashtag to be searched
+    twitter_stream = Stream(_get_twitter_api(), MyListener(db_server, db_name))
+
+    # twitter_stream.filter(place!=None)
+    twitter_stream.filter(locations=location)
+
+
+if __name__ == '__main__':
+    token_idx = '0'
+    db_name = 'tweet_mixed'
+    geo_only = False
+    db_ip = os.getenv("COUCH_DB_IP")
+    client = dbOperations(db_ip=db_ip, db_port=5984,
+                          db_username="ccc58", db_passwd="ccc58", databases=["tweet_mixed"])
+    if len(sys.argv) == 2:
+
+        city = sys.argv[1]
+        location = _get_coordinates_base_city(city)
+        print("Harvest streaming tweets from {} ...".format(city))
+        get_twitters_stream(location, client, db_name)
+    elif len(sys.argv) > 2:
+        city = sys.argv[1]
+        api_get = sys.argv[2]
+        location = _get_coordinates_base_city(city)
+        print("Harvest streaming tweets from {} with API {} ...".format(city, api_get))
+        get_twitters_stream(location, client, db_name)
+    else:
+        print("Please input city name and API number...")
+
