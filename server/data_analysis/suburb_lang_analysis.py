1bab328f4573e712ba5af82675dd06a5f2996576
Fredy-Zhang <fredy.zhang.au@gmail.com>
2021-05-25 05:14:44 +1000

    add the sub analysis scripts

--- server/data_analysis/suburb_lang_analysis.py
+++ server/data_analysis/suburb_lang_analysis.py
@@ -6,7 +6,7 @@ Analysis the Covid disscusion rate within every different city.
 4. return the JSON to Flask as API.
 """
 import re
-import os
+import os, json
 import couchdb
 import logging
 
@@ -96,46 +96,143 @@ server = dbOperations(db_ip, db_port=5984, db_username="ccc58", db_passwd="ccc58
 #----------------------------------------------------------------#
 #    Functions Definitions
 #----------------------------------------------------------------#
-def generate_map_function(city_name):
+def points_map_function():
     mapFunction = '''function (doc) {
-                        if(doc.place.name == "XXX")
-                        emit(doc.id, [doc.place.name, doc.text, doc.lang]);
-                    }'''.replace("XXX", city_name)
+                      if(doc.geo.type == "Point")
+                      emit(doc.geo.coordinates, [doc.place.name, doc.text, doc.lang]);
+                    }'''
     return mapFunction
 
-def get_data_from_db_views(db_name, city_name): # db_name, design_doc_name, view_name, map_function
+def get_data_from_db_views(db_name): # db_name, design_doc_name, view_name, map_function
     try:
-        mapFunction = generate_map_function(city_name)
-        server._create_view(db_name, design_doc_name= str(city_name)+"_DOC",
-                            view_name= str(city_name)+"_info", map_function=mapFunction)
+        mapFunction = points_map_function()
+        # createView( db_Au, "Point_DOC", "point_info", mapFunction )
+        server._create_view(db_name, design_doc_name= "Point_DOC",
+                            view_name= "point_info", map_function=mapFunction)
     except:
         print("[*] view has already exist!")
-    view_str = city_name+"_DOC/"+city_name+"_info"
-    return server._get_view_data(db_name, view_str)
+    view_str = "Point_DOC/point_info"
+    return server._get_view_data(db_name, view_str)  ## return key: coordinate, value:[place, text]
 
-def process_data(input):
-    total_count = 0 
-    count = 0
-    for item in get_data_from_db_views(db_name=input[0], city_name=input[1]):
-        total_count += 1
-        if len(item.value[1]) == 0:
-            continue
-        if "covid" in item.value[1].lower():
-            count += 1
-    return (input[1], count/total_count)
+def load_suburbs_polygon(path):
+    with open(path, "r") as f:
+        json_data = json.load(f)
+    sub_data = {}
+    for i in json_data["Features"]:
+        sub_data[i["sa4_code"]] = i["geometry"]["coordinates"][0]
+    return sub_data
+
+def get_score_mapping(filePath= "AFINN.txt"):
+    """
+    Parser the AFINN.txt file, extracting the word and socres.
+    return:
+           @dict: {short:, longer:} 
+           eg. long: no fun : 
+    """
+    scores = {"long":{}, "short":{}}
+    with open(filePath, "r") as txt_f:
+        for idx, line in enumerate(txt_f):
+            line_l = line.split()
+            if len(line_l) == 2: scores["short"][line_l[0]] = int(line_l[1])
+            else:
+                scores["long"][" ".join(line_l[:-1])] = int(line_l[-1])
+    #log_info("total length of afinn.txt: {}".format(idx))
+    return scores
+
+def generate_dict(ll):
+    dict = {}
+    for i in ll:
+        dict[i] = {"lang":{}, "emotion":0}
+    return dict
+#----------------------------------------------------------------#
+#    Polygon definitions
+#----------------------------------------------------------------#
+ADE_polygon = load_suburbs_polygon("config/sa4_ade.json")
+BRI_polygon = load_suburbs_polygon("config/sa4_bri.json")
+MEL_polygon = load_suburbs_polygon("config/sa4_mel.json")
+PER_polygon = load_suburbs_polygon("config/sa4_per.json")
+SYD_polygon = load_suburbs_polygon("config/sa4_syd.json")
+
+AFINN = {}
+AFINN = get_score_mapping()
+
+WHOLE_LIST = list(ADE_polygon.keys()) + list(BRI_polygon.keys()) + \
+             list(MEL_polygon.keys()) + list(PER_polygon.keys()) + \
+             list(SYD_polygon.keys())
+WHOLE_DATA = dict(ADE_polygon, **BRI_polygon, **MEL_polygon, \
+                  **PER_polygon, **SYD_polygon)
+WHOLE_DICT = generate_dict(WHOLE_LIST)
+#----------------------------------------------------------------#
+#    Functions Definitions
+#----------------------------------------------------------------#
+def calculate_sentiment_scores(text, AFINN):
+    """
+    Eg, "good -> match
+        good!!.. -> match
+        good!@.. -> not match
+        no fun -> match
+    """
+    scores = 0
+    head_symbols = ("'", '"')
+    tail_symbols = ("!","?",",",".","'",'"') ## these consider as exact match, when it appears with word.
+    context = text.lower()
+    context = context.replace("\n", "")
+    # match the symbols not in tail_symbols list, remove these word.
+    ## At first, match the long word, then remove them from the text.
+    for long_word in AFINN["long"].keys():
+        if long_word in context:
+            scores += AFINN["long"][long_word]
+            context.replace(long_word, "")
+    ## then, catch the single word from head with " ', or tail with ("!","?",",",".","'",'"')
+    for word in context.split():
+        # if word.isalpha():
+        # if word in AFINN["short"].keys(): scores += AFINN["short"][word]
+        if bool(re.search(r'\d', word)): continue # if string contains digital, continue.
+        elif word.startswith(head_symbols): # "hello if remove ", the whole data become alpha, match.
+            if bool(re.search(r"\W",word[1:])):
+                if word.endswith(tail_symbols):
+                    if len(set(re.findall(r"\W", word[1:])).difference(set(tail_symbols))) > 0: 
+                        continue
+                    else:
+                        word = re.findall("^[a-z]*", word[1:])[0]
+                else: continue
+            else: 
+                word = word[1:]
+        elif word.endswith(tail_symbols):
+            if len(set(re.findall(r"\W", word)).difference(set(tail_symbols))) > 0: 
+                continue
+            else:
+                word = re.findall("^[a-z]*", word)[0]
+        if word in AFINN["short"].keys(): 
+            scores += AFINN["short"][word]
+    return scores
+
+def find_belong_to_sub(coordinate):
+    import numpy as np
+    result = {}
+    for key in WHOLE_DATA.keys():
+        result[np.sum((np.array(WHOLE_DATA[key]) - np.array(coordinate))**2)] = key
+    return result[min(list(result.keys()))]
 
 def _covid_rate_result(db_name):
-    cities = ["Sydney", "Melbourne", "Brisbane", "Perth (WA)", "Adelaide"]
-    inputs = []
-    for i in range(len(cities)):
-        inputs.append([db_name, cities[i]])
-    with Pool(5) as p:
-        result = p.map(process_data, inputs)
-    
-    return result
+    for item in get_data_from_db_views(db_name):
+        coordinate, text, lang = item.key, item.value[1], item.value[2]
+        key = find_belong_to_sub(coordinate[::-1])
+        if lang not in WHOLE_DICT[key]["lang"].keys():
+            WHOLE_DICT[key]["lang"][lang] = 1
+        else:
+            WHOLE_DICT[key]["lang"][lang] += 1
+        WHOLE_DICT[key]["emotion"] += calculate_sentiment_scores(text, AFINN)
+    return WHOLE_DICT
 
 
 if __name__ == "__main__":
     cities = ["Sydney", "Melbourne", "Brisbane", "Perth (WA)", "Adelaide"]
     #get_data_from_db_views("sydney", cities[0])
-    print(_covid_rate_result("whole_au"))
+    # for i in get_data_from_db_views("whole_au"):
+    #     print(i)
+    #print(load_suburbs_polygon("config/sa4_ade.json")["401"])
+    #print(WHOLE_DATA["507"])
+    with open("suburb_lang_emotion.json", "w") as f:
+        json.dump(_covid_rate_result("whole_au"), f)
+    
--- server/flask/flaskApp/appServer.py
+++ server/flask/flaskApp/appServer.py
@@ -2,18 +2,18 @@ from . import app
 from flask import request, url_for, jsonify, make_response, abort
 import os
 
-from data_analysis import city_sentiment_analysis, city_covid_analysis
+from data_analysis import city_sentiment_analysis, city_covid_analysis, suburb_lang_analysis
 
 db_name = "whole_au"
 
-@app.route("/api/?city=sentiments", methods=['Get', 'POST'])
+@app.route("/api/city=sentiments", methods=['Get', 'POST'])
 def return_sentiments_result():
     return city_sentiment_analysis._sentiment_result(db_name)
 
-@app.route('/api/?city=covidrates', methods=['GET', 'POST'])
+@app.route('/api/city=covidrates', methods=['GET', 'POST'])
 def return_covid_rates():
     return city_covid_analysis._covid_rate_result(db_name)
 
-@app.route('/api/?suburb=lang', methods=['GET', 'POST'])
+@app.route('/api/suburb=lang', methods=['GET', 'POST'])
 def return_suburb_lang_rate():
-    return "GET --------- OPERATIONS"
+    return suburb_lang_analysis._covid_rate_result(db_name)
